18600a32a8f16b7ab0f0a1ee988946db41d2c83c
NikhilH91 <117922542+NikhilH91@users.noreply.github.com>
2024-03-03 14:50:27 +0530

    server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT,HOST, () => {
-  console.log(Running on http://${HOST}:${PORT}');
+app.listen(PORT, HOST , () => {
+  console.log('Running on http://${HOST}:${PORT}');
   });
